acbf336ff2236b251526e711ab1894219419fa75
KenLiau <itl.kglab@gmail.com>
2024-08-10 19:50:17 +0800

    完成Navigation實作

---
+++ app/src/main/java/com/itl/kglab/notedatabase/ui/EditNoteFragment.kt
@@ -0,0 +1,24 @@
+package com.itl.kglab.notedatabase.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.itl.kglab.notedatabase.R
+import com.itl.kglab.notedatabase.databinding.FragmentEditNoteBinding
+
+class EditNoteFragment : Fragment() {
+
+    private var _binding: FragmentEditNoteBinding? = null
+    private val binding: FragmentEditNoteBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEditNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/itl/kglab/notedatabase/ui/MainListFragment.kt
@@ -0,0 +1,32 @@
+package com.itl.kglab.notedatabase.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.itl.kglab.notedatabase.R
+import com.itl.kglab.notedatabase.databinding.FragmentMainListBinding
+
+class MainListFragment : Fragment() {
+
+    private var _binding: FragmentMainListBinding? = null
+    private val binding: FragmentMainListBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainListBinding.inflate(inflater, container, false)
+        initView()
+        return binding.root
+    }
+
+    private fun initView() {
+        binding.textView.setOnClickListener {
+            findNavController().navigate(R.id.action_mainListFragment_to_editNoteFragment)
+        }
+    }
+
+}
